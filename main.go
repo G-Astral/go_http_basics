f812862fa1427f44903e2ee840406dcb57e0848c
G-Astral <super.anakonda2001@yandex.ru>
2025-06-26 00:12:36 +0300

    Тестовый запуск сервака перенесен в отдельную папку. В main реализована запись данных в JSON. Создан JSON

--- main.go
+++ main.go
@@ -1,38 +1,68 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request)  {
-	var msg string
+type User struct {
+	ID		int		`json:"id"`
+	Name	string	`json:"name"`
+	Age		int		`json:"age"`
+}
 
-	switch r.URL.Path {
-	case "/":
-		msg = "Привет, мир!"
-	case "/hello":
-		msg = "Привет, пользователь!"
-	default:
-		http.NotFound(w, r)
+var (
+	users 	[]User
+	nextID 	int = 1
+)
+
+func createUserHandler(w http.ResponseWriter, r * http.Request)  {
+	// Если заданный метод не является методом создания юзера - выходим
+	if r.Method != http.MethodPost {
+		http.Error(w, "Метод не поддерживается", http.StatusMethodNotAllowed)
 		return
 	}
 
-	if msg != "" {
-		fmt.Fprintln(w, msg)
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "Ошибка чтения JSON", http.StatusBadRequest)
+		return
 	}
+
+	user.ID = nextID
+	nextID++
+	users = append(users, user)
+
+	fmt.Println("Сохраняем пользователей в файл...")
+	err = saveToFile()
+	if err != nil {
+		http.Error(w, "Ошибка записи в файл", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
+}
+
+func saveToFile() error {
+	file, err := os.Create("users.json")
+	fmt.Println("Файл успешно открыт для записи")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	return json.NewEncoder(file).Encode(users)
 }
 
 func main()  {
-	// http.HandleFunc("/", handler) — регистрирует функцию-обработчик
-	// handler для пути /. То есть: если кто-то откроет http://localhost:8080/ —
-	// Go вызовет твою функцию handler.
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/hello", handler)
-
+	http.HandleFunc("/user", createUserHandler)
 	fmt.Println("Сервер запущен на http://localhost:8080")
-
-	// http.ListenAndServe(":8080", nil) — запускает сервер.
-	// Он слушает порт 8080 и обрабатывает все HTTP-запросы
-	http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Ошибка запуска сервера:", err)
+	}
 }
